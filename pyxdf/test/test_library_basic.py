012295d2e2de93252854d48f279cb8daf6315050
Tristan Stenner <ttstenner@gmail.com>
2019-04-28 12:56:12 +0200

    Add tests for _read_varlen_int

--- pyxdf/test/test_library_basic.py
+++ pyxdf/test/test_library_basic.py
@@ -1,5 +1,7 @@
 import pyxdf
+import pyxdf.pyxdf
 import pytest
+import io
 
 
 #%% test
@@ -10,3 +12,15 @@ def test_load_xdf_present():
     """
     assert(hasattr(pyxdf, 'load_xdf'))
 
+
+def test_read_varlen_int():
+    """"""
+
+    def vla(data: bytes):
+        return pyxdf.pyxdf._read_varlen_int(io.BytesIO(data))
+
+    assert vla(b'\x01\xfd') == 0xfd
+    assert vla(b'\x04\xfd\x12\x00\x34') == 0x340012fd
+    assert vla(b'\x08\xfd\x12\x00\x34\x12\x34\x56\x78') == 0x78563412340012fd
+    with pytest.raises(RuntimeError):
+        vla(b'\x00')

4c7c96784604a4a942c64443062b1b9e39e981f7
Clemens Brunner <clemens.brunner@gmail.com>
2019-10-08 15:53:34 +0200

    Finding breaks is only possible if there are at least 2 time stamps

--- pyxdf/pyxdf.py
+++ pyxdf/pyxdf.py
@@ -484,7 +484,7 @@ def _clock_sync(streams,
             # if requested, this is only for cases where "everything goes
             # wrong" during recording note that this is a fancy feature that
             # is not needed for normal XDF compliance.
-            if handle_clock_resets:
+            if handle_clock_resets and len(clock_times) > 1:
                 # First detect potential breaks in the synchronization data;
                 # this is only necessary when the importer should be able to
                 # deal with recordings where the computer that served a

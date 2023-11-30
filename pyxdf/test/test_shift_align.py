1050094817c0723cb420419a243a9d213299632c
agricolab <robertsadresse@gmx.de>
2023-11-30 09:16:43 +0100

    Test edges

--- pyxdf/test/test_shift_align.py
+++ pyxdf/test/test_shift_align.py
@@ -36,3 +36,23 @@ def test_shift_align_jittered():
     for x, y, xhat, yhat in zip(jittered_timestamps, old_timeseries, old_timestamps, new_timeseries):
         print(f"{x:3.4f} -> {xhat:3.4f} = {y[0]:3.0f} / {yhat[0]:3.0f} ")
         assert y == yhat
+
+def test_shift_align_edges():
+    print("\n==================")
+    new_timestamps = np.arange(0.5, 1.7, 0.01)
+    # idx = np.argwhere(old_timestamps<1.2)[-1][0]+1 
+    idx = len(old_timestamps)
+    new_timeseries = _shift_align(old_timestamps[:idx], old_timeseries, new_timestamps)    
+    for xhat, yhat in zip(new_timestamps, new_timeseries):        
+        delta = xhat-old_timestamps
+        idx_old = np.argmin(np.abs(delta))
+        x = old_timestamps[idx_old]        
+        if xhat < 1.00 or xhat > 1.5001:
+            y = np.nan
+        else:
+            y = old_timeseries[idx_old,0]
+        print(f"{x:3.4f} -> {xhat:3.4f} = {y:3.0f} / {yhat[0]:3.0f} ")
+        if (np.isnan(y)):
+            assert np.isnan(yhat[0])
+        else:
+            assert y == yhat[0]

ac25a6ef741e8efa0bfa597a176e4445cd3412b7
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-12-06 06:28:18 -0700

    Address @cbrnr PR review.

--- src/pyxdf/__init__.py
+++ src/pyxdf/__init__.py
@@ -1,4 +1,4 @@
 from .__version__ import __version__ as __version__
 from .pyxdf import load_xdf, match_streaminfos, resolve_streams
 
-__all__ = [load_xdf, resolve_streams, match_streaminfos]
+__all__ = ["load_xdf", "resolve_streams", "match_streaminfos"]

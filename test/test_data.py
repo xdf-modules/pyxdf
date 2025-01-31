372d615d3840b237c53262112b465420547e9cb6
Jamie Forth <j.forth@gold.ac.uk>
2025-01-31 01:32:40 +0000

    Update empty_streams test
    * new empty_streams.xdf with correct sample_count
    
    * add float comparison tolerance levels
    
    * explicitly test effect of synchronize_clocks
    
    * dejittering has no significant effect because ground-truth
      time-stamps have zero jitter, but keep as a test parameter to show
      that dejittering does not cause problem in idealised cases

--- test/test_data.py
+++ test/test_data.py
@@ -168,95 +168,76 @@ def test_empty_streams_file(synchronize_clocks, dejitter_timestamps):
     assert len(streams) == 4
 
     # Stream ID: 1
-    i = 1
-    assert streams[i]["info"]["name"][0] == "Data stream: test stream 0 counter"
-    assert streams[i]["info"]["type"][0] == "data"
+    i = 2
+    assert streams[i]["info"]["name"][0] == "ctrl"
+    assert streams[i]["info"]["type"][0] == "control"
     assert streams[i]["info"]["channel_count"][0] == "1"
-    assert streams[i]["info"]["channel_format"][0] == "int32"
-    assert streams[i]["info"]["source_id"][0] == "test_stream.py:525352:0"
-    assert streams[i]["info"]["nominal_srate"][0] == "1.000000000000000"
+    assert streams[i]["info"]["nominal_srate"][0] == "0.000000000000000"
+    assert streams[i]["info"]["channel_format"][0] == "string"
+    assert streams[i]["info"]["source_id"][0] == "kassia"
+    assert streams[i]["info"]["nominal_srate"][0] == "0.000000000000000"
     assert streams[i]["info"]["version"][0] == "1.100000000000000"
-    assert streams[i]["info"]["created_at"][0] == "401309.0364671120"
-    assert streams[i]["info"]["uid"][0] == "25e1bd13-340f-499c-bb91-5d1e75cec535"
+    assert streams[i]["info"]["created_at"][0] == "91684.87631725401"
+    assert streams[i]["info"]["uid"][0] == "4740b9ba-d45d-4e2b-9a4b-ee966d9b56df"
     assert streams[i]["info"]["session_id"][0] == "default"
     assert streams[i]["info"]["hostname"][0] == "kassia"
     assert streams[i]["info"]["v4address"][0] is None
-    assert streams[i]["info"]["v4data_port"][0] == "16576"
-    assert streams[i]["info"]["v4service_port"][0] == "16575"
+    assert streams[i]["info"]["v4data_port"][0] == "16572"
+    assert streams[i]["info"]["v4service_port"][0] == "16572"
     assert streams[i]["info"]["v6address"][0] is None
     assert streams[i]["info"]["v6data_port"][0] == "0"
     assert streams[i]["info"]["v6service_port"][0] == "0"
     assert streams[i]["info"]["desc"][0]["manufacturer"][0] == "pylsltools"
-    channels = streams[i]["info"]["desc"][0]["channels"][0]
-    assert channels["channel"][0]["label"][0] == "ch:00"
-    assert channels["channel"][0]["type"][0] == "misc"
 
     # Info added by pyxdf
     assert streams[i]["info"]["stream_id"] == 1
-    assert streams[i]["info"]["effective_srate"] == pytest.approx(1)
-    assert streams[i]["info"]["segments"] == [(0, 9)]
+    assert streams[i]["info"]["effective_srate"] == 0
+    assert streams[i]["info"]["segments"] == [(0, 0)]
 
     # Footer
-    assert streams[i]["footer"]["info"]["first_timestamp"][0] == "401340.7970979316"
-    assert streams[i]["footer"]["info"]["last_timestamp"][0] == "401350.7970979316"
-    # Sample count is off by one
-    assert streams[i]["footer"]["info"]["sample_count"][0] == "9"
+    assert streams[i]["footer"]["info"]["first_timestamp"][0] == "91725.014004246"
+    assert streams[i]["footer"]["info"]["last_timestamp"][0] == "91725.014004246"
+    assert streams[i]["footer"]["info"]["sample_count"][0] == "1"
     first_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][0]
-    assert first_clock_offset["time"][0] == "401322.6950535755"
-    assert first_clock_offset["value"][0] == "-3.67984757758677e-05"
+    assert first_clock_offset["time"][0] == "91716.691545932"
+    assert first_clock_offset["value"][0] == "-1.889200211735442e-05"
     last_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][-1]
-    assert last_clock_offset["time"][0] == "401372.696774303"
-    assert last_clock_offset["value"][0] == "-3.553100395947695e-05"
+    assert last_clock_offset["time"][0] == "91746.69261952251"
+    assert last_clock_offset["value"][0] == "-3.837950498564169e-05"
 
     # Time-series data
-    s = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]], dtype=np.int32)
-    t = np.array(
-        [
-            401340.79706958,
-            401341.79706948,
-            401342.79706938,
-            401343.79706927,
-            401344.79706917,
-            401345.79706907,
-            401346.79706896,
-            401347.79706886,
-            401348.79706875,
-            401349.79706865,
-        ]
-    )
+    s = [['{"state": 2}']]
+    t = np.array([91725.01400425])
 
-    np.testing.assert_equal(streams[i]["time_series"], s)
+    assert streams[i]["time_series"] == s
     np.testing.assert_allclose(streams[i]["time_stamps"], t)
 
     # Clock offsets
-    np.testing.assert_equal(streams[i]["clock_times"][0], 401322.6950535755)
-    np.testing.assert_equal(streams[i]["clock_values"][0], -3.67984757758677e-05)
-    np.testing.assert_equal(streams[i]["clock_times"][-1], 401372.696774303)
-    np.testing.assert_equal(streams[i]["clock_values"][-1], -3.553100395947695e-05)
+    assert streams[i]["clock_times"][0] == pytest.approx(91716.691545932, abs=1e-6)
+    assert streams[i]["clock_values"][0] == pytest.approx(0, abs=1e-4)
+    assert streams[i]["clock_times"][-1] == pytest.approx(91746.69261952251, abs=1e-6)
+    assert streams[i]["clock_values"][-1] == pytest.approx(0, abs=1e-4)
 
     # Stream ID: 2
-    i = 0
-    assert streams[i]["info"]["name"][0] == "Empty data stream: test stream 0 counter"
+    i = 3
+    assert streams[i]["info"]["name"][0] == "Empty marker stream: test stream 0 counter"
     assert streams[i]["info"]["type"][0] == "data"
     assert streams[i]["info"]["channel_count"][0] == "1"
-    assert streams[i]["info"]["channel_format"][0] == "float32"
-    assert streams[i]["info"]["source_id"][0] == "test_stream.py:525257:0"
-    assert streams[i]["info"]["nominal_srate"][0] == "1.000000000000000"
+    assert streams[i]["info"]["channel_format"][0] == "string"
+    assert streams[i]["info"]["source_id"][0] == "test_stream.py:191748:0"
+    assert streams[i]["info"]["nominal_srate"][0] == "0.000000000000000"
     assert streams[i]["info"]["version"][0] == "1.100000000000000"
-    assert streams[i]["info"]["created_at"][0] == "401285.3015719900"
-    assert streams[i]["info"]["uid"][0] == "30608cb9-b177-420d-9d60-3ce0f07949af"
+    assert streams[i]["info"]["created_at"][0] == "91696.18816467900"
+    assert streams[i]["info"]["uid"][0] == "6f7e0288-10b8-4f48-89f8-0381ce20922f"
     assert streams[i]["info"]["session_id"][0] == "default"
     assert streams[i]["info"]["hostname"][0] == "kassia"
     assert streams[i]["info"]["v4address"][0] is None
     assert streams[i]["info"]["v4data_port"][0] == "16574"
-    assert streams[i]["info"]["v4service_port"][0] == "16573"
+    assert streams[i]["info"]["v4service_port"][0] == "16574"
     assert streams[i]["info"]["v6address"][0] is None
     assert streams[i]["info"]["v6data_port"][0] == "0"
     assert streams[i]["info"]["v6service_port"][0] == "0"
     assert streams[i]["info"]["desc"][0]["manufacturer"][0] == "pylsltools"
-    channels = streams[i]["info"]["desc"][0]["channels"][0]
-    assert channels["channel"][0]["label"][0] == "ch:00"
-    assert channels["channel"][0]["type"][0] == "misc"
 
     # Info added by pyxdf
     assert streams[i]["info"]["stream_id"] == 2
@@ -268,48 +249,48 @@ def test_empty_streams_file(synchronize_clocks, dejitter_timestamps):
     assert streams[i]["footer"]["info"]["last_timestamp"][0] == "0"
     assert streams[i]["footer"]["info"]["sample_count"][0] == "0"
     first_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][0]
-    assert first_clock_offset["time"][0] == "401322.695044571"
-    assert first_clock_offset["value"][0] == "-3.130998811684549e-05"
+    assert first_clock_offset["time"][0] == "91716.691513728"
+    assert first_clock_offset["value"][0] == "-2.540599962230772e-05"
     last_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][-1]
-    assert last_clock_offset["time"][0] == "401372.6966923515"
-    assert last_clock_offset["value"][0] == "-1.937249908223748e-05"
+    assert last_clock_offset["time"][0] == "91746.69276649199"
+    assert last_clock_offset["value"][0] == "-2.026499714702368e-05"
 
     # Time-series data
-    s = np.zeros((0, 1), dtype=np.int32)
+    s = []
     t = np.array([], dtype=np.float64)
 
     np.testing.assert_equal(streams[i]["time_series"], s)
-    np.testing.assert_allclose(streams[i]["time_stamps"], t)
+    np.testing.assert_equal(streams[i]["time_stamps"], t)
 
     # Clock offsets
-    np.testing.assert_equal(streams[i]["clock_times"][0], 401322.69504457095)
-    np.testing.assert_equal(streams[i]["clock_values"][0], -3.130998811684549e-05)
-    np.testing.assert_equal(streams[i]["clock_times"][-1], 401372.6966923515)
-    np.testing.assert_equal(streams[i]["clock_values"][-1], -1.9372499082237482e-05)
+    assert streams[i]["clock_times"][0] == pytest.approx(91716.691513728, abs=1e-6)
+    assert streams[i]["clock_values"][0] == pytest.approx(0, abs=1e-4)
+    assert streams[i]["clock_times"][-1] == pytest.approx(91746.692766492, abs=1e-6)
+    assert streams[i]["clock_values"][-1] == pytest.approx(0, abs=1e-4)
 
     # Stream ID: 3
-    i = 2
-    assert streams[i]["info"]["name"][0] == "Empty marker stream: test stream 0 counter"
+    i = 0
+    assert streams[i]["info"]["name"][0] == "Empty data stream: test stream 0 counter"
     assert streams[i]["info"]["type"][0] == "data"
     assert streams[i]["info"]["channel_count"][0] == "1"
-    assert streams[i]["info"]["channel_format"][0] == "string"
-    assert streams[i]["info"]["source_id"][0] == "test_stream.py:525304:0"
-    assert streams[i]["info"]["nominal_srate"][0] == "0.000000000000000"
-    assert streams[i]["info"]["stream_id"] == 3
-    assert streams[i]["info"]["segments"] == []
-    assert streams[i]["info"]["clock_segments"] == []
+    assert streams[i]["info"]["channel_format"][0] == "float32"
+    assert streams[i]["info"]["source_id"][0] == "test_stream.py:191790:0"
+    assert streams[i]["info"]["nominal_srate"][0] == "1.000000000000000"
     assert streams[i]["info"]["version"][0] == "1.100000000000000"
-    assert streams[i]["info"]["created_at"][0] == "401297.3977076210"
-    assert streams[i]["info"]["uid"][0] == "3ece2528-0c45-4e6f-9a00-7eb1a7f7bd84"
+    assert streams[i]["info"]["created_at"][0] == "91699.83395166400"
+    assert streams[i]["info"]["uid"][0] == "ff430a18-9954-43f5-bb5f-f5589e3aa6a2"
     assert streams[i]["info"]["session_id"][0] == "default"
     assert streams[i]["info"]["hostname"][0] == "kassia"
     assert streams[i]["info"]["v4address"][0] is None
     assert streams[i]["info"]["v4data_port"][0] == "16575"
-    assert streams[i]["info"]["v4service_port"][0] == "16574"
+    assert streams[i]["info"]["v4service_port"][0] == "16575"
     assert streams[i]["info"]["v6address"][0] is None
     assert streams[i]["info"]["v6data_port"][0] == "0"
     assert streams[i]["info"]["v6service_port"][0] == "0"
     assert streams[i]["info"]["desc"][0]["manufacturer"][0] == "pylsltools"
+    channels = streams[i]["info"]["desc"][0]["channels"][0]
+    assert channels["channel"][0]["label"][0] == "ch:00"
+    assert channels["channel"][0]["type"][0] == "misc"
 
     # Info added by pyxdf
     assert streams[i]["info"]["stream_id"] == 3
@@ -321,72 +302,93 @@ def test_empty_streams_file(synchronize_clocks, dejitter_timestamps):
     assert streams[i]["footer"]["info"]["last_timestamp"][0] == "0"
     assert streams[i]["footer"]["info"]["sample_count"][0] == "0"
     first_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][0]
-    assert first_clock_offset["time"][0] == "401322.6951932265"
-    assert first_clock_offset["value"][0] == "-2.594449324533343e-05"
+    assert first_clock_offset["time"][0] == "91716.6915301265"
+    assert first_clock_offset["value"][0] == "-2.211050014011562e-05"
     last_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][-1]
-    assert last_clock_offset["time"][0] == "401372.6966891775"
-    assert last_clock_offset["value"][0] == "-1.620649709366262e-05"
+    assert last_clock_offset["time"][0] == "91746.69269425601"
+    assert last_clock_offset["value"][0] == "-1.128000440075994e-05"
 
     # Time-series data
-    s = []
+    s = np.zeros((0, 1), dtype=np.int32)
     t = np.array([], dtype=np.float64)
 
     np.testing.assert_equal(streams[i]["time_series"], s)
-    np.testing.assert_allclose(streams[i]["time_stamps"], t)
+    np.testing.assert_equal(streams[i]["time_stamps"], t)
 
     # Clock offsets
-    np.testing.assert_equal(streams[i]["clock_times"][0], 401322.6951932265)
-    np.testing.assert_equal(streams[i]["clock_values"][0], -2.5944493245333433e-05)
-    np.testing.assert_equal(streams[i]["clock_times"][-1], 401372.69668917754)
-    np.testing.assert_equal(streams[i]["clock_values"][-1], -1.620649709366262e-05)
+    assert streams[i]["clock_times"][0] == pytest.approx(91716.6915301265, abs=1e-6)
+    assert streams[i]["clock_values"][0] == pytest.approx(0, abs=1e-4)
+    assert streams[i]["clock_times"][-1] == pytest.approx(91746.69269425601, abs=1e-6)
+    assert streams[i]["clock_values"][-1] == pytest.approx(0, abs=1e-4)
 
     # Stream ID: 4
-    i = 3
-    assert streams[i]["info"]["name"][0] == "ctrl"
-    assert streams[i]["info"]["type"][0] == "control"
+    i = 1
+    assert streams[i]["info"]["name"][0] == "Data stream: test stream 0 counter"
+    assert streams[i]["info"]["type"][0] == "data"
     assert streams[i]["info"]["channel_count"][0] == "1"
-    assert streams[i]["info"]["nominal_srate"][0] == "0.000000000000000"
-    assert streams[i]["info"]["channel_format"][0] == "string"
-    assert streams[i]["info"]["source_id"][0] == "kassia"
-    assert streams[i]["info"]["nominal_srate"][0] == "0.000000000000000"
+    assert streams[i]["info"]["channel_format"][0] == "int32"
+    assert streams[i]["info"]["source_id"][0] == "test_stream.py:191695:0"
+    assert streams[i]["info"]["nominal_srate"][0] == "1.000000000000000"
     assert streams[i]["info"]["version"][0] == "1.100000000000000"
-    assert streams[i]["info"]["created_at"][0] == "401261.9233872890"
-    assert streams[i]["info"]["uid"][0] == "eb31d8f6-b57a-4e45-bc5a-fa98573d6503"
+    assert streams[i]["info"]["created_at"][0] == "91690.55328314200"
+    assert streams[i]["info"]["uid"][0] == "bc60b7bb-e632-407c-b3db-e42f9cad4179"
     assert streams[i]["info"]["session_id"][0] == "default"
     assert streams[i]["info"]["hostname"][0] == "kassia"
     assert streams[i]["info"]["v4address"][0] is None
     assert streams[i]["info"]["v4data_port"][0] == "16573"
-    assert streams[i]["info"]["v4service_port"][0] == "16572"
+    assert streams[i]["info"]["v4service_port"][0] == "16573"
     assert streams[i]["info"]["v6address"][0] is None
     assert streams[i]["info"]["v6data_port"][0] == "0"
     assert streams[i]["info"]["v6service_port"][0] == "0"
     assert streams[i]["info"]["desc"][0]["manufacturer"][0] == "pylsltools"
+    channels = streams[i]["info"]["desc"][0]["channels"][0]
+    assert channels["channel"][0]["label"][0] == "ch:00"
+    assert channels["channel"][0]["type"][0] == "misc"
 
     # Info added by pyxdf
     assert streams[i]["info"]["stream_id"] == 4
-    assert streams[i]["info"]["effective_srate"] == 0
-    assert streams[i]["info"]["segments"] == [(0, 0)]
+    assert streams[i]["info"]["effective_srate"] == pytest.approx(1)
+    assert streams[i]["info"]["segments"] == [(0, 9)]
 
     # Footer
-    assert streams[i]["footer"]["info"]["first_timestamp"][0] == "401340.597121355"
-    assert streams[i]["footer"]["info"]["last_timestamp"][0] == "401340.597121355"
-    assert streams[i]["footer"]["info"]["sample_count"][0] == "0"
+    assert streams[i]["footer"]["info"]["first_timestamp"][0] == "91725.21394789348"
+    assert streams[i]["footer"]["info"]["last_timestamp"][0] == "91735.21394789348"
+    assert streams[i]["footer"]["info"]["sample_count"][0] == "10"
     first_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][0]
-    assert first_clock_offset["time"][0] == "401322.69519626"
-    assert first_clock_offset["value"][0] == "-2.898101229220629e-05"
+    assert first_clock_offset["time"][0] == "91716.6915717245"
+    assert first_clock_offset["value"][0] == "-1.94335007108748e-05"
     last_clock_offset = streams[i]["footer"]["info"]["clock_offsets"][0]["offset"][-1]
-    assert last_clock_offset["time"][0] == "401372.6968414925"
-    assert last_clock_offset["value"][0] == "-2.722250064834952e-05"
+    assert last_clock_offset["time"][0] == "91746.69274602149"
+    assert last_clock_offset["value"][0] == "-3.694550105137751e-05"
 
     # Time-series data
-    s = [['{"state": 2}']]
-    t = np.array([401340.59709634])
+    s = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]], dtype=np.int32)
+    t = np.array(
+        [
+            91725.21394789,
+            91726.21394789,
+            91727.21394789,
+            91728.21394789,
+            91729.21394789,
+            91730.21394789,
+            91731.21394789,
+            91732.21394789,
+            91733.21394789,
+            91734.21394789,
+        ]
+    )
 
-    assert streams[i]["time_series"] == s
-    np.testing.assert_allclose(streams[i]["time_stamps"], t)
+    np.testing.assert_equal(streams[i]["time_series"], s)
+    if synchronize_clocks:
+        np.testing.assert_almost_equal(streams[i]["time_stamps"], t, decimal=4)
+    else:
+        np.testing.assert_almost_equal(streams[i]["time_stamps"], t, decimal=8)
+
+    # Dejittering should have no effect because timestamps have minimal jitter.
+    assert np.std(np.diff(streams[i]["time_stamps"])) < 6e-11
 
     # Clock offsets
-    np.testing.assert_equal(streams[i]["clock_times"][0], 401322.69519626)
-    np.testing.assert_equal(streams[i]["clock_values"][0], -2.8981012292206287e-05)
-    np.testing.assert_equal(streams[i]["clock_times"][-1], 401372.6968414925)
-    np.testing.assert_equal(streams[i]["clock_values"][-1], -2.7222500648349524e-05)
+    assert streams[i]["clock_times"][0] == pytest.approx(91716.6915717245, abs=1e-6)
+    assert streams[i]["clock_values"][0] == pytest.approx(0, abs=1e-4)
+    assert streams[i]["clock_times"][-1] == pytest.approx(91746.6927460215, abs=1e-6)
+    assert streams[i]["clock_values"][-1] == pytest.approx(0, abs=1e-4)

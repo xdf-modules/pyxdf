281ceec7978e4192ebc0b780ea747b424ac21e9b
Jamie Forth <j.forth@gold.ac.uk>
2025-02-13 20:53:44 +0000

    Parameterize small segment indices tests
    Also change exhaustive test to not re-test the small examples.

--- test/test_segment_indices.py
+++ test/test_segment_indices.py
@@ -5,46 +5,21 @@ import pytest
 from pyxdf.pyxdf import _find_segment_indices
 
 
-def test_segments_size_0_nbreaks_0():
-    b_breaks = np.array([])
+@pytest.mark.parametrize(
+    "b_breaks, expected",
+    [
+        (np.array([]), [(0, 0)]),
+        (np.array([False]), [(0, 1)]),
+        (np.array([True]), [(0, 0), (1, 1)]),
+        (np.array([False, False]), [(0, 2)]),
+        (np.array([True, False]), [(0, 0), (1, 2)]),
+        (np.array([False, True]), [(0, 1), (2, 2)]),
+        (np.array([True, True]), [(0, 0), (1, 1), (2, 2)]),
+    ],
+)
+def test_segments(b_breaks, expected):
     segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 0)]
-
-
-def test_segments_size_1_nbreaks_0():
-    b_breaks = np.array([False])
-    segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 1)]
-
-
-def test_segments_size_1_nbreaks_1():
-    b_breaks = np.array([True])
-    segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 0), (1, 1)]
-
-
-def test_segments_size_2_nbreaks_0():
-    b_breaks = np.array([False, False])
-    segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 2)]
-
-
-def test_segments_size_2_nbreaks_1a():
-    b_breaks = np.array([True, False])
-    segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 0), (1, 2)]
-
-
-def test_segments_size_2_nbreaks_1b():
-    b_breaks = np.array([False, True])
-    segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 1), (2, 2)]
-
-
-def test_segments_size_2_nbreaks_2():
-    b_breaks = np.array([True, True])
-    segments = _find_segment_indices(b_breaks)[0]
-    assert segments == [(0, 0), (1, 1), (2, 2)]
+    assert segments == expected
 
 
 @pytest.mark.parametrize(
@@ -62,15 +37,15 @@ def test_segments_size_2_nbreaks_2():
                 n_breaks,
                 list(itertools.combinations(range(0, size), n_breaks)),
             )
-            # All sequences of length 0 to 5
-            for size in range(0, 6)
+            # All sequences of length 3 to 5, continuing from the tests above.
+            for size in range(3, 6)
             # All numbers of breaks between 0 and size
             for n_breaks in range(0, size + 1)
         ]
         for breaks in [np.array(breaks, dtype=int) for breaks in breaks_list]
     ],
 )
-def test_segments_all(size, n_breaks, breaks, expected):
+def test_segments_exhaustive(size, n_breaks, breaks, expected):
     b_breaks = np.full(size, False)
     b_breaks[breaks] = True
     segments = _find_segment_indices(b_breaks)[0]

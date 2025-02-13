25afb0c4abedda65e5fb0ae909d1bd3d32de704d
Jamie Forth <j.forth@gold.ac.uk>
2025-02-13 20:14:01 +0000

    Parameterize some _detect_breaks tests improve readability

--- test/test_detect_breaks.py
+++ test/test_detect_breaks.py
@@ -53,39 +53,30 @@ def test_detect_breaks_samples():
     assert breaks.size == len(time_stamps) - 1
 
 
-def test_detect_breaks_gap_in_negative():
-    time_stamps = [-4, 1, 2, 3, 4]
+@pytest.mark.parametrize("threshold_seconds", [0.1, 1, 2])
+@pytest.mark.parametrize("offset", [-10, -1, 0, 10])
+def test_detect_breaks_monotonic(offset, threshold_seconds):
+    time_stamps = [0] + [2, 3, 4, 5] + [8, 9, 10]
+    time_stamps = np.array(time_stamps) + offset
     stream = MockStreamData(time_stamps=time_stamps, tdiff=1)
-    # if diff > 1 -> 1
-    b_breaks = _detect_breaks(stream, threshold_seconds=1, threshold_samples=0)
+    b_breaks = _detect_breaks(
+        stream,
+        threshold_seconds=threshold_seconds,
+        threshold_samples=0,
+    )
     breaks = np.where(b_breaks)[0]
-    assert breaks.size == 1
-    assert breaks[0] == 0
-    time_stamps = [-4, -2, -1, 0, 1, 2, 3, 4]
-    stream = MockStreamData(time_stamps=time_stamps, tdiff=1)
-    # if diff > 1 -> 1
-    b_breaks = _detect_breaks(stream, threshold_seconds=1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == 1
-    assert breaks[0] == 0
-    # if diff > 0.1 -> 7
-    b_breaks = _detect_breaks(stream, threshold_seconds=0.1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == len(time_stamps) - 1
-
-
-def test_detect_breaks_gap_in_positive():
-    time_stamps = [1, 3, 4, 5, 6]
-    stream = MockStreamData(time_stamps=time_stamps, tdiff=1)
-    # if diff > 1 -> 1
-    b_breaks = _detect_breaks(stream, threshold_seconds=1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == 1
-    assert breaks[0] == 0
-    # if diff > 0.1 -> 4
-    b_breaks = _detect_breaks(stream, threshold_seconds=0.1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == len(time_stamps) - 1
+    if threshold_seconds == 0.1:
+        # if diff > 0.1 -> 8
+        assert breaks.size == len(time_stamps) - 1
+    elif threshold_seconds == 1:
+        # if diff > 1 -> 2
+        assert breaks.size == 2
+        assert breaks[0] == 0
+        assert breaks[1] == 4
+    elif threshold_seconds == 2:
+        # if diff > 2 -> 1
+        assert breaks.size == 1
+        assert breaks[0] == 4
 
 
 # Non-monotonic timeseries data: segment at negative time-intervals or positive
@@ -101,41 +92,29 @@ def test_detect_breaks_reverse():
     assert breaks.size == len(time_stamps) - 1
 
 
-def test_detect_breaks_non_monotonic_gap_in_negative():
-    time_stamps = [-4, -7, -6, -5, -3, -2, -1, 0]
+@pytest.mark.parametrize("threshold_seconds", [0.1, 1, 2])
+@pytest.mark.parametrize("offset", [-10, -1, 0, 10])
+def test_detect_breaks_non_monotonic(offset, threshold_seconds):
+    time_stamps = [3] + [0, 1, 2] + [4, 5, 6] + [5]
+    time_stamps = np.array(time_stamps) + offset
     stream = MockStreamData(time_stamps=time_stamps, tdiff=1)
-    # if diff < 0 or diff > 1 -> 2
-    b_breaks = _detect_breaks(stream, threshold_seconds=1, threshold_samples=0)
+    b_breaks = _detect_breaks(
+        stream,
+        threshold_seconds=threshold_seconds,
+        threshold_samples=0,
+    )
     breaks = np.where(b_breaks)[0]
-    assert breaks.size == 2
-    assert breaks[0] == 0
-    assert breaks[1] == 3
-    # if diff < 0 or diff > 2 -> 1
-    b_breaks = _detect_breaks(stream, threshold_seconds=2, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == 1
-    assert breaks[0] == 0
-    # if diff < 0 or diff > 0.1 -> 7
-    b_breaks = _detect_breaks(stream, threshold_seconds=0.1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == len(time_stamps) - 1
-
-
-def test_detect_breaks_non_monotonic_gap_in_positive():
-    time_stamps = [4, 1, 2, 3, 5, 6, 7, 8]
-    stream = MockStreamData(time_stamps=time_stamps, tdiff=1)
-    # if diff < 0 or diff > 1 -> 2
-    b_breaks = _detect_breaks(stream, threshold_seconds=1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == 2
-    assert breaks[0] == 0
-    assert breaks[1] == 3
-    # if diff < 0 or diff > 2 -> 1
-    b_breaks = _detect_breaks(stream, threshold_seconds=2, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == 1
-    assert breaks[0] == 0
-    # if diff < 0 or diff > 0.1 -> 7
-    b_breaks = _detect_breaks(stream, threshold_seconds=0.1, threshold_samples=0)
-    breaks = np.where(b_breaks)[0]
-    assert breaks.size == len(time_stamps) - 1
+    if threshold_seconds == 0.1:
+        # if diff < 0 or diff > 0.1 -> 8
+        assert breaks.size == len(time_stamps) - 1
+    elif threshold_seconds == 1:
+        # if diff < 0 or diff > 1 -> 3
+        assert breaks.size == 3
+        assert breaks[0] == 0
+        assert breaks[1] == 3
+        assert breaks[2] == 6
+    elif threshold_seconds == 2:
+        # if diff < 0 or diff > 2 -> 2
+        assert breaks.size == 2
+        assert breaks[0] == 0
+        assert breaks[1] == 6

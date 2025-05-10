fe67ea9e0e517655bd1a3bc79b918f8e527998f8
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-05-10 12:27:22 -0400

    playback - add pytest

---
+++ test/test_lsl_playback.py
@@ -0,0 +1,19 @@
+import importlib
+from pathlib import Path
+
+import pytest
+
+
+path = Path(__file__).parents[1] / "example-files" / "minimal.xdf"
+
+
+@pytest.mark.skipif(not path.exists(), reason="File not found.")
+@pytest.mark.skipif(
+    not importlib.util.find_spec("pylsl"), reason="requires the pylsl library"
+)
+def test_lsl_playback():
+    """
+    Test the LSL playback functionality.
+    """
+    from pyxdf.cli.playback_lsl import main as playback_main
+    playback_main(str(path), playback_speed=10.0, loop=False, wait_for_consumer=False)
